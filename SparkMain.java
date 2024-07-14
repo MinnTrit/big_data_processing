34a03d54b4bedc5332fd3e3851e920c27f7305d7
MinnTrit <151976884+MinnTrit@users.noreply.github.com>
2024-07-14 16:31:15 +0700

    SparkMain.java

--- SparkMain.java
+++ SparkMain.java
@@ -5,22 +5,23 @@ import java.util.ArrayList;
 public class SparkMain {
 	public static void main (String[] args) {
 		//Initialize the date processor
-		String startDate = "2022-04-02";
-		String endDate = "2022-04-03";
+		//This code is used to test to process 1 day only
+		String startDate = "2022-04-01";
+		String endDate = "2022-04-01";
 		DateProcessor dateProcessor = new DateProcessor(startDate, endDate);
 		ArrayList<String> dateList = dateProcessor.getDatelist();
 		
 		//Initialize the data frame processor
-		String folderPath = "D:\\Data Engineer Course\\Day4";
+		String folderPath = "";
 		DataFrameProcessor dataFrameProcessor = new DataFrameProcessor(folderPath, dateList);
 		Dataset<Row> df = dataFrameProcessor.readingFrame();
 		df = dataFrameProcessor.dataFrameTransform(df);
 		df = dataFrameProcessor.gettingTaste(df);
 		df = dataFrameProcessor.gettingMostwatched(df);
 		df = dataFrameProcessor.gettingActiveness(df);
-		String username = "spark";
-		String password = "Bautroixanh12345";
-		String tableName = "data_engineer";
+		String username = "";
+		String password = "";
+		String tableName = "";
 		dataFrameProcessor.saveToDatabase(df, username, password, tableName);
 	}	
 }
